ff983c948d515b2bb27547d161323bf41e22854c
pgulati14 <codebind@LAPTOP-L71GSGF9.localdomain>
2021-11-28 22:55:54 +0530

    bycrypt and Signin Sign up page functionality added

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  #get "home", to: "home#index"
+  get "/", to: "home#index"
   # get "menus", to: "menus#index"
   resources :menus
   resources :menu_items
+  resources :users
+  post "/signin" => "sessions#create", as: :sessions
+  get "/signin" => "sessions#new", as: :new_sessions
+
 end
