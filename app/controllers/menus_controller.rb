b3141787b5e8c32a9b50ff32eda84a2a20267a21
pgulati14 <codebind@LAPTOP-L71GSGF9.localdomain>
2021-11-28 20:30:58 +0530

    menu create and show

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -2,4 +2,17 @@ class MenusController < ApplicationController
   def index
         render plain: Menu.all.map { |menu| menu.to_pleasant_string }.join("\n")
   end
+  def show
+    id = params[:id]
+    menu_items = MenuItem.where("menu_id=?", id)
+    render plain: menu_items.all.map { |menu_item| menu_item.to_pleasant_string }.join("\n")
+  end
+  def create
+    id = params[:menu_id]
+    name = params[:menu_name]
+    new_menu = Menu.create!(
+      menu_id: id,
+      menu_name: name,
+    )
+  end
 end
